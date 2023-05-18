f43e9ea6644c1d24e04ba9981007c87746ed60ba
gherreramusica <132178396+gherreramusica@users.noreply.github.com>
2023-05-17 22:32:57 -0500

    Update hde.js

--- hde.js
+++ hde.js
@@ -1,37 +1,14 @@
-// Funcion para obtener la hora y fecha
-function updateTime() {
-    const now = new Date();
-    const date = now.toLocaleDateString();
-    const time = now.toLocaleTimeString();
-    document.getElementById('txt').innerHTML = `${time} - ${date}`;
-  }
-  
-  updateTime();
-  setInterval(updateTime, 1000);
 
-
-// Funcion para el music player
-
-  var audio = document.getElementById('music');
-
-  function playAudio() { 
-    audio.play(); 
-  }
-  
-  function pauseAudio() { 
-    audio.pause(); 
-  };
-  
-// Funcion para el nav desplegable
-
-  function myFunction() {
-    var x = document.getElementById("menu");
-    if (x.className === "menu") {
-      x.className += " show";
-    } else {
-      x.className = "menu";
-    }
-  }
-
-
-
+var toggle = document.getElementById('toggle');
+var cerrar = document.getElementById('close');
+var vertical_nav = document.getElementById('vertical_nav');
+toggle.addEventListener('click', () => {
+  vertical_nav.classList.toggle('show');
+  toggle.classList.toggle('hide');
+  cerrar.classList.toggle('show');
+});
+cerrar.addEventListener('click', () => {
+  vertical_nav.classList.toggle('show');
+  toggle.classList.toggle('hide');
+  cerrar.classList.toggle('show');
+});
